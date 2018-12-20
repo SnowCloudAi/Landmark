d9733a35bf007ec78ce9a3f0809ddf94b7160b56
Wu Yuxiang <40594739+wuyxiquanquan@users.noreply.github.com>
2018-12-20 14:33:33 +0800

    WING LOSS

--- networks/newLoss.py
+++ networks/newLoss.py
@@ -1,13 +1,12 @@
 import torch
-import math
 
-
-def wingLoss(pred, target, w=10, epsilon=2):
+def wingLoss(pred, target, w=10.0, epsilon=2.0):
     w, epsilon = torch.tensor([w]).cuda(), torch.tensor([epsilon]).cuda()
     dis = torch.abs_(pred - target)
-    isSmall = dis < w
-    isLarge = dis >= w
+    isSmall = dis <= w
+    isLarge = dis > w
     small_loss = w * torch.log((isSmall * dis) / epsilon + 1)
     large_loss = isLarge * dis - w * (1 - torch.log(1 + w / epsilon))
     loss = small_loss + large_loss * isLarge
+    loss = torch.mean(torch.sum(loss, dim=1),dim=0)
     return loss
